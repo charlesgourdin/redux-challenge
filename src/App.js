9df8ef5d1fbf220897ce756bad4abcd940a4fa5a
Charles Gourdin <gourdin.charles@gmail.com>
2019-12-02 09:38:27 +0100

    Création de Test.js - ready pour la quête REDUX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <Test/>
     </div>
   );
 }
